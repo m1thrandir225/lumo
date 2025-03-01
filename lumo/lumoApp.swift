fb2c73b538bcc3bd3f641b71752acee19907f071
Sebastijan Zindl <sebastijanzindl@protonmail.com>
2025-03-01 18:19:27 +0100

    Initial Commit

---
+++ lumo/lumoApp.swift
@@ -0,0 +1,17 @@
+//
+//  lumoApp.swift
+//  lumo
+//
+//  Created by Sebastijan Zindl on 1.3.25.
+//
+
+import SwiftUI
+
+@main
+struct lumoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
